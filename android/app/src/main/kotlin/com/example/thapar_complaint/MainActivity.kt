93ab1f1585e0ec52e0e9911d21f33b64b38c3827
Tanmay Deshkar <tdeshkar_be21@gamil.com>
2022-06-29 01:22:30 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/thapar_complaint/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.thapar_complaint
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:get/get.dart';
+import 'login_page.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  return runApp(Myapp());
+}
+
+class Myapp extends StatelessWidget {
+  const Myapp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      home: LoginPage(),
+    );
+  }
+}
